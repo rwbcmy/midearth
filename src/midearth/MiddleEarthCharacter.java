e028e5074327dd0fcf530bcd029476d11ab1f62d
rwbcmy <rwbcmy@umsystem.edu>
2025-03-05 15:59:38 -0600

    Implemented MiddleEarthCharacter abstract class with fields and methods

--- src/midearth/MiddleEarthCharacter.java
+++ src/midearth/MiddleEarthCharacter.java
@@ -2,4 +2,48 @@ package midearth;
 
 public abstract class MiddleEarthCharacter {
 
+	protected String name;
+	protected double health;
+	protected double power;
+	
+	public MiddleEarthCharacter(String name, double health, double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	public abstract boolean attack(MiddleEarthCharacter target);
+	
+	public abstract String getRace();
+	
+	public void displayInfo() {
+		System.out.println("Name: " + name);
+		System.out.println("Health: " + health);
+		System.out.println("Power: " + power);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getHealth() {
+		return health;
+	}
+	
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	
+	public double getPower() {
+		return power;
+	}
+	
+	public void setPower(double power) {
+		this.power = power;
+	}
+	
 }
